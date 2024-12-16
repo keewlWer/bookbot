716377fadd7c4e64366661ebe68ba50109be18c0
keewlWer <kuzdru@gmail.com>
2024-12-16 04:38:33 +0000

    Add function get_chars_dict

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ def main():
     print(text)
     number_of_words = get_number_of_words(text)
     print(number_of_words)
+    number_of_characters = get_chars_dict(text.lower())
+    print(number_of_characters)
+
+def get_chars_dict(lowered_text):
+    chars = {}
+    for c in lowered_text:
+        if c in chars:
+            chars[c] += 1
+        else:
+            chars[c] = 1
+    return chars
 
 def get_number_of_words(text):
     return len(text.split())
